1250fbf7e1efbd7f40232be938db24a507f24a9e
Aleafan <ae.afanasov@yandex.com>
2021-05-18 12:06:29 +0300

    Rework class components to functional components with state hooks

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,80 +1,58 @@
-import React, { Component } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-class Education extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            school: '',
-            faculty: '',
-            title: '',
-            dates: '',
-          };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+const Education = (props) => {
+    const [school, setSchool] = useState('');
+    const [faculty, setFaculty] = useState('');
+    const [title, setTitle] = useState('');
+    const [dates, setDates] = useState('');
 
-    handleChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value,
-        })
-    }
+    const lang = props.language;
+    const placeholderDates = (lang === 'ru'? 'мм-гггг по мм-гггг' : 'yyyy-mm to yyyy-mm');
 
-    handleSubmit(e) {
-        e.preventDefault();
-    }
-
-    render() {
-        let {school, faculty, title, dates} = this.state;
-        let lang = this.props.language;
-        let placeholderDates = lang === 'ru'? 'мм-гггг по мм-гггг' : 'yyyy-mm to yyyy-mm';
-
-        if (this.props.edit) {
-            return (
-                <div className='section'>
-                    <form onSubmit={this.handleSubmit}>
-                        <label>
-                            {lang === 'ru'? 'Учебное учреждение:' : 'School/university name:'}
-                            <input type='text' value={school} name='school' onChange={this.handleChange}/>
-                        </label>
-                        <label>
-                            {lang === 'ru'? 'Факультет:' : 'Faculty/department:'}
-                            <input type='text' value={faculty} name='faculty' onChange={this.handleChange}/>
-                        </label>
-                        <label>
-                            {lang === 'ru'? 'Специальность:' : 'Graduate as:'}
-                            <input type='text' value={title} name='title' onChange={this.handleChange}/>
-                        </label>
-                        <label>
-                            {lang === 'ru'? 'Сроки обучения:' : 'Dates of study:'}
-                            <input type='text' value={dates} name='dates' onChange={this.handleChange}
-                                placeholder={placeholderDates}/>
-                        </label>
-                    </form>
-
-                    <button className='btn-delete' type='button' onClick={this.props.delete} id={this.props.id}
-                            data-section='educationIds'>
-                        <FontAwesomeIcon icon={faTrash} /> 
-                        {lang === 'ru'? 'Удалить' : 'Delete entry'}
-                    </button>
-                    
-                </div>
-            );
-        }
+    if (props.edit) {
         return (
             <div className='section'>
-                <div className='flex-section'>
-                    <p className='dates'>{dates}</p>
-                    <div>
-                        <h3>{title}</h3>
-                        <p><u>{school}</u></p>
-                        <p><em>{faculty}</em></p>
-                    </div>
-                </div>
+                <form onSubmit={(e) => e.preventDefault()}>
+                    <label>
+                        {lang === 'ru'? 'Учебное учреждение:' : 'School/university name:'}
+                        <input type='text' value={school} onChange={(e) => setSchool(e.target.value)}/>
+                    </label>
+                    <label>
+                        {lang === 'ru'? 'Факультет:' : 'Faculty/department:'}
+                        <input type='text' value={faculty} onChange={(e) => setFaculty(e.target.value)}/>
+                    </label>
+                    <label>
+                        {lang === 'ru'? 'Специальность:' : 'Graduate as:'}
+                        <input type='text' value={title} onChange={(e) => setTitle(e.target.value)}/>
+                    </label>
+                    <label>
+                        {lang === 'ru'? 'Сроки обучения:' : 'Dates of study:'}
+                        <input type='text' value={dates} onChange={(e) => setDates(e.target.value)}
+                               placeholder={placeholderDates}/>
+                    </label>
+                </form>
+
+                <button className='btn-delete' type='button' onClick={props.delete} id={props.id}>
+                    <FontAwesomeIcon icon={faTrash} /> 
+                    {lang === 'ru'? 'Удалить' : 'Delete entry'}
+                </button>
             </div>
         );
     }
+    return (
+        <div className='section'>
+            <div className='flex-section'>
+                <p className='dates'>{dates}</p>
+                <div>
+                    <h3>{title}</h3>
+                    <p><u>{school}</u></p>
+                    <p><em>{faculty}</em></p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Education
--- src/components/Profession.js
+++ src/components/Profession.js
@@ -1,86 +1,64 @@
-import React, { Component } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-class Profession extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            company: '',
-            position: '',
-            dates: '',
-            responsibilities: '',
-          };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+const Profession = (props) => {
+    const [company, setCompany] = useState('');
+    const [position, setPosition] = useState('');
+    const [dates, setDates] = useState('');
+    const [responsibilities, setResponsibilities] = useState('');
 
-    handleChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value,
-        })
-    }
+    const lang = props.language;
+    const placeholderDates = (lang === 'ru'? 'мм-гггг по мм-гггг' : 'yyyy-mm to yyyy-mm');
 
-    handleSubmit(e) {
-        e.preventDefault();
-    }
-
-    render() {
-        let {company, position, dates, responsibilities} = this.state;
-        let lang = this.props.language;
-        let placeholderDates = lang === 'ru'? 'мм-гггг по мм-гггг' : 'yyyy-mm to yyyy-mm';
-
-        if (this.props.edit) {
-            return (
-                <div className='section'>
-                    <form onSubmit={this.handleSubmit}>
-                        <label>
-                            {lang === 'ru'? 'Компания:' : 'Company:'}
-                            <input type='text' value={company} name='company' onChange={this.handleChange}/>
-                        </label>
-                        <label>
-                            {lang === 'ru'? 'Должность:' : 'Position:'}
-                            <input type='text' value={position} name='position' onChange={this.handleChange}/>
-                        </label>
-                        <label>
-                            {lang === 'ru'? 'Сроки работы:' : 'Dates of work:'}
-                            <input type='text' value={dates} name='dates' onChange={this.handleChange}
-                                placeholder={placeholderDates} />
-                        </label>
-                        <label>
-                            {lang === 'ru'? 'Должностные обязанности:' : 'Responsibilities:'}
-                            <textarea rows='4' value={responsibilities} name='responsibilities' onChange={this.handleChange}/>
-                        </label>
-                    </form>
-
-                    <button className='btn-delete' type='button' onClick={this.props.delete} id={this.props.id} 
-                            data-section='professionIds'>
-                        <FontAwesomeIcon icon={faTrash} /> 
-                        {lang === 'ru'? 'Удалить' : 'Delete entry'}
-                    </button>
-                    
-                </div>
-            );
-        }
+    if (props.edit) {
         return (
             <div className='section'>
-                <div className='flex-section'>
-                    <p className='dates'>{dates}</p>
-                    <div>
-                        <h3>{position}</h3>
-                        <p><u>{company}</u></p>
-                        {responsibilities &&
-                            <div>
-                                <p id='responsibilities-header'>
-                                    {lang === 'ru'? 'Должностные обязанности:' : 'Responsibilities:'}
-                                </p>
-                                <p id='responsibilities'>{responsibilities}</p>
-                            </div>}
-                    </div>
-                </div>
+                <form onSubmit={(e) => e.preventDefault()}>
+                    <label>
+                        {lang === 'ru'? 'Компания:' : 'Company:'}
+                        <input type='text' value={company} onChange={(e) => setCompany(e.target.value)}/>
+                    </label>
+                    <label>
+                        {lang === 'ru'? 'Должность:' : 'Position:'}
+                        <input type='text' value={position} onChange={(e) => setPosition(e.target.value)}/>
+                    </label>
+                    <label>
+                        {lang === 'ru'? 'Сроки работы:' : 'Dates of work:'}
+                        <input type='text' value={dates} onChange={(e) => setDates(e.target.value)}
+                               placeholder={placeholderDates} />
+                    </label>
+                    <label>
+                        {lang === 'ru'? 'Должностные обязанности:' : 'Responsibilities:'}
+                        <textarea rows='4' value={responsibilities} onChange={(e) => setResponsibilities(e.target.value)}/>
+                    </label>
+                </form>
+
+                <button className='btn-delete' type='button' onClick={props.delete} id={props.id}>
+                    <FontAwesomeIcon icon={faTrash} /> 
+                    {lang === 'ru'? 'Удалить' : 'Delete entry'}
+                </button>
             </div>
         );
     }
+    return (
+        <div className='section'>
+            <div className='flex-section'>
+                <p className='dates'>{dates}</p>
+                <div>
+                    <h3>{position}</h3>
+                    <p><u>{company}</u></p>
+                    {responsibilities &&
+                        <div>
+                            <p id='responsibilities-header'>
+                                {lang === 'ru'? 'Должностные обязанности:' : 'Responsibilities:'}
+                            </p>
+                            <p id='responsibilities'>{responsibilities}</p>
+                        </div>}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Profession
